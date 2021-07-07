77739a20f112de7a8ae38df97ca87dc896fec994
Leonardo <leonardosawi12@gmail.com>
2021-07-07 14:19:09 -0500

    Update server.py

--- server.py
+++ server.py
@@ -66,7 +66,7 @@ class Battlesnake(object):
         # This function is called when a game your snake was in ends.
         # It's purely for informational purposes, you don't have to make any decisions here.
         data = cherrypy.request.json
-
+        global_variables.GAME_ON = False
         print("END")
         return "ok"
 
